915131b293dac4d75ee2993fb477fcfc817e2dce
John <jacv050@gmail.com>
2020-07-27 21:44:23 +0200

    Alpha test pose reference

--- test_pose_rebuild.py
+++ test_pose_rebuild.py
@@ -1,15 +1,43 @@
 import argparse
-import pytorch3d
+#import pytorch3d
 import utils.qutils as qutils
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import sys
+
+#%matplotlib notebook
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['savefig.dpi'] = 80
+mpl.rcParams['figure.dpi'] = 80
+
+def plot_pose(pose_points, title=""):
+    x = []
+    y = []
+    z = []
+    for point in pose_points:
+      x.append(point[0])
+      y.append(point[1])
+      z.append(point[2])
+    # Sample points uniformly from the surface of the mesh.
+    fig = plt.figure(figsize=(5, 5))
+    ax = Axes3D(fig)
+    ax.scatter3D(np.array(x), np.array(z), -np.array(y))
+    ax.set_xlabel('x')
+    ax.set_ylabel('z')
+    ax.set_zlabel('y')
+    ax.set_title(title)
+    ax.view_init(190, 30)
+    plt.show()
 
 #pose_reference list of lists
 #pose_prediction list/array
 #World rotation to bone space
 def rebuild_pose_from_quaternion(pose_reference, pose_prediction):
-    hands = ["left_hand", "right_hand"]
+    #hands = ["left_hand", "right_hand"]
+    hands = ["right_hand"]
 
     pose_points = []
 
@@ -19,32 +47,46 @@ def rebuild_pose_from_quaternion(pose_reference, pose_prediction):
 
       #Root rotation
       root   = next(iterator) #pos = 0 orien = 1 (p,y,r)
-      root_rot     = qutils.euler2quaternion(root[1], ['Y', 'X', 'Z'])
-      root_pos     = qutils.from_float_array([0, 1, 1, 1]) #Test
+      root_rot     = qutils.euler2quaternion(root[1], ['Y', 'X', 'Z']) #FROM UNREAL
+      #root_rot     = qutils.euler2quaternion(np.array(root[1])[[2,0,1]], ['X', 'Y', 'Z'])
+      root_pos     = qutils.quaternion.from_float_array([0, 1, 1, 1]) #Test
       root_rotated = root_rot * root_pos * root_rot.inverse() #Root vector
+
+      pose_points.append(qutils.quaternion.as_float_array(root_pos)[1:4])
+
+      #TEST DELETE
+      next(iterator)
+      next(iterator)
+      next(iterator)
+      next(iterator)
       for finger in iterator:
         #q = root_rot * root_pos * root_rot.inverse() #* qutils.from_float_array([1, 0, 0, 0])
         #Quaternion ROTATION
-        qrot = root_rot
-        #rotated VECTOR
         vec  = root_pos
+        #rotated VECTOR
+        q = root_rot
         for joint in finger:
-          #Quaternion ROTATION
+          #Quaternion ROTATION ORIGINAL angles are 'r' 'p' 'y' swaped into 'p' 'y' 'r'
           phalange_rot     = qutils.euler2quaternion(joint[1], ['Y', 'X', 'Z'])
-          #Quaternion VECTOR
-          phalange_pos     = qutils.from_float_array([0, 0.5, 0.5, 0.5]) #Test joint[0]
-          #Quaternion VECTOR
-          phalange_rotated = phalange_rot * phalange_pos * phalange_rot.inverse() #Phalange vector
-          #Quaternion ROT * ROT
+          #phalange_rot     = qutils.euler2quaternion(np.array(joint[1])[[2,0,1]], ['X', 'Y', 'Z'])
           q  = phalange_rot * q # q-ref
           qn = q.normalized()
+          #Quaternion VECTOR
+          phalange_pos     = qutils.quaternion.from_float_array([0, 0.5, 0.5, 0.5]) #Test joint[0]
+          #Quaternion VECTOR
+          phalange_rotated = qn * phalange_pos * qn.inverse() #Phalange vector
+          #Quaternion ROT * ROT
           #World to Bone space ROT
-          bone_space_rot = qn.inverse() * qutils.from_float_array(pose_prediction[ijoint*4:ijoint*4+4]) * qn #Pose reference - World to bone space
+          #bone_space_rot = qn.inverse() * qutils.quaternion.from_float_array(pose_prediction[ijoint*4:ijoint*4+4]) * qn #Pose reference - World to bone space
           #Apply bone space rot to phalange
-          bone = bone_space_rot * phalange_rotated * bone_space_rot.inverse() #Apply axis neuron angle read
+          bone = phalange_rotated
+          #bone = bone_space_rot * phalange_rotated * bone_space_rot.inverse() #Apply axis neuron angle read
           #Final point
-          vec = qutils.as_float_array(vec + bone)
-          pose_points.append(vec)
+          #vec = qutils.quaternion.as_float_array(vec + bone)[1:4]
+          vec = vec + bone
+          point = qutils.quaternion.as_float_array(vec)[1:4]
+
+          pose_points.append(point)
 
           ijoint = ijoint + 1
 
@@ -87,9 +129,13 @@ if __name__ == "__main__":
     with open(ARGS_.output_error) as f:
       output_error = json.load(f)
 
-    pose = rebuild_pose_from_quaternion(pose_reference, output_error['output'])
+    #pose = rebuild_pose_from_quaternion(pose_reference, output_error['output'])
+    pose = rebuild_pose_from_quaternion(pose_reference, output_error['output_ground_truth'])
 
-    plt.plot(pose)
+
+    print(len(pose))
+
+    plot_pose(pose, title="Pose")
     plt.show()
 
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
