Quaternion output normalized and fixed radians in euler2quaternion

dataset/unrealhands_otf.py
@@ -76,14 +76,14 @@ class UnrealHands(Dataset):
         kinf = kin[l]
       else:
         kinf = kin
-
       #Convert to quaternion from kinematics
       q = None
       #Quaternion from not indexed specific angles
       if quaternion and not indexed:
-        q = np.zeros(int(kin.shape[0]/3*4), dtype=np.float64)
+        q = np.zeros(int(kin.shape[0]/3*4), dtype=np.float64) #bones angles divided 3 * 4 -> 64
         for i in range(int(kin.size/3)):
-          qaux = qutils.quaternion.as_float_array(qutils.euler2quaternion([kin[i*3], kin[i*3+1], kin[i*3+2]]))
+          #Correct pyr -> rpy
+          qaux = qutils.quaternion.as_float_array(qutils.euler2quaternion(np.radians([kin[i*3+2], kin[i*3], kin[i*3+1]]), ['Y', 'X', 'Z']))
           q[i*4] = qaux[0]
           q[i*4+1] = qaux[1]
           q[i*4+2] = qaux[2]
@@ -205,17 +205,12 @@ class UnrealHands(Dataset):
   def process_threaded(self, p):
     path_cloud = self.raw_paths_processed[p]
     #TODO Parameterize
-    old_joints = False
+    old_joints = False #OLD_JOINTS
     labels = None
-    if old_joints:
+    if old_joints: #DEFAULT FALSE
       path_joints = (path_cloud[:len(path_cloud)-3] + "json").replace(self.cloud_folder, "joints")
-      #LOG.info("Processing cloud {0} out of {1}".format(p, len(self.raw_paths_processed)))
-
-      #LOG.info(path_cloud)
-      #LOG.info(path_joints)
       hands_ = self.read_joints_json2(path_joints)
       labels = hands_["left_hand"]+hands_["right_hand"]
-    #print(labels)
 
     with open(self.raw_paths_processed[p], 'rb') as f:
       print(self.raw_paths_processed[p])

loss/geodesic_distance_weighted.py
@@ -11,7 +11,13 @@ class GeodesicDistanceWeightedLoss(nn.Module):
     self.eps = 1e-6
 
   def forward(self, inputs, targets):
-    prod=inputs*targets
+    #Normalizing
+    magnitude=torch.zeros(int(targets.shape[0]), requires_grad=False, device=torch.cuda.current_device())
+    #for i in range(int(magnitude.shape[0]/4)):
+    #  aux=torch.pow(inputs[i*4:i*4+4],2)
+    #  magnitude[i*4:i*4+4]=torch.sqrt(torch.sum(aux))
+    #prod=torch.mul(torch.div(inputs,magnitude),targets)
+    prod=torch.mul(inputs,targets)
     inner=torch.zeros(int(targets.shape[0]/4), requires_grad=False)
     for i in range(inner.shape[0]):
       aux = torch.sum(prod[i*4:i*4+4])
@@ -19,6 +25,6 @@ class GeodesicDistanceWeightedLoss(nn.Module):
       #inner[i]=torch.pow(torch.sum(prod[i*4:i*4+4]),2)
     #inner[0] = inner[0]*1.5 #Weight
     #inner=torch.sum(inner)
-    return self.mae(inner, torch.ones(int(targets.shape[0]/4)))
+    return self.mse(inner, torch.ones(int(targets.shape[0]/4)))
     #return torch.sqrt(self.mse(inner, torch.ones(int(targets.shape[0]/4))) + self.eps)
     #return torch.abs(torch.sub(targets.shape[0]/4, inner))